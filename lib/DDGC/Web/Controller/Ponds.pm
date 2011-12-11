b72fd93361b544f43872606ad4d7be47d21d7070
canardee-io <canardee.io@gmx.com>
2011-12-11 15:39:53 +0000

    Ponds - base for integration of Github high scores

---
+++ lib/DDGC/Web/Controller/Ponds.pm
@@ -0,0 +1,57 @@
+package DDGC::Web::Controller::Ponds;
+use Data::Dumper;
+use Moose;
+use namespace::autoclean;
+
+use DDGC::Config;
+use Github::Score;
+
+
+# use base qw/Catalyst::Controller/;
+# 
+# 	__PACKAGE__->config(
+# 		  actions => {
+# 			'repo_scores' => { Path => '/github/repo-scores' },
+# 	,
+# 	);
+# 
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head2 repo_scores
+
+=cut
+##
+## Queries:
+## /github/repo-scores
+## /github/repo-scores/months
+## /github/repos/scores
+## The question is how specific to make the url
+## 
+sub repo_scores : Chained('/base') :PathPart('ponds/github/scores') :CaptureArgs(0) {  
+my ( $self, $c ) = @_;
+
+
+}
+
+sub ponds : Chained('/') :PathPart('ponds') :CaptureArgs(0) {
+my ( $self, $c ) = @_;
+
+}
+
+sub github : Chained('ponds') :Args(0) {  
+my ( $self, $c ) = @_;
+
+
+}
+
+
+sub sourceforge : Chained('ponds') :Args(0) {  
+my ( $self, $c ) = @_;
+
+
+}
+
+#####
+1;
+
+
